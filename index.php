37bd7b5504d858ba37d1a22b708d24422c83885a
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2024-05-16 20:37:37 -0500

    Updating credentials!!!
    Signed-off-by: Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
+// Database connection configuration
+$servername = "localhost";
+$username = "root";
+$password = "SuperSecretPassword123!"; // This is a hardcoded password
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
 
-$password = "(*&FASV*(PFJNTR)!%^";
-
-
-
-echo $password;
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
